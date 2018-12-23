ce4e2fd17b31b217cae553be7213a17dd40dfd6e
MatthewCisler <matthew@cisler.com>
2018-12-23 00:41:28 -0600

    update Main.java
    declared some variables in the same line they were used in. Also removed some clutter and added some validation.

--- Main.java
+++ Main.java
@@ -3,22 +3,15 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int lowNumber;
-        int highNumber;
-        int firstRand;
-        int secondRand;
         int answer;
         int userAnswer;
         int total;
-        System.out.println("Hello World!");
 
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
 
-        System.out.print("Please enter the lowest value you would like to multiply today. ");
-        lowNumber = sc.nextInt();
-        System.out.print("Please enter the highest value you would like to multiply today. ");
-        highNumber = sc.nextInt();
+        int lowNumber = getInt(sc, "Please enter the lowest value you would like to multiply today. ");        
+        int highNumber = getCorrectIntValue(sc, "Please enter the highest value you would like to multiply today. ", lowNumber);
 
         System.out.print("How many math problems would you like to do? ");
         total = sc.nextInt();
@@ -26,8 +19,8 @@ public class Main {
         boolean value = false;
         for (int i = 0; i < total; ++i) {
 
-            firstRand = rand.nextInt(highNumber) + lowNumber;
-            secondRand = rand.nextInt(highNumber) + lowNumber;
+            int firstRand = rand.nextInt(highNumber - lowNumber) + lowNumber;
+            int secondRand = rand.nextInt(highNumber - lowNumber) + lowNumber;
 
             while (!value) {
                 userAnswer = 0;
@@ -47,5 +40,37 @@ public class Main {
             userAnswer = 0;
             value = false;
         }
+    }
+
+    private static int getInt(Scanner sc, String prompt)    {
+        int i = 0;
+        boolean valid = false;
+        while(!valid)   {
+            System.out.print(prompt);
+            if(sc.hasNextInt()) {
+                i = sc.nextInt();
+                valid = true;
+            }
+            else{
+                System.out.println("Error! Invalid value. Try again.");
+            }
+            sc.nextLine();
+        }
+        return i;
+    }
+
+    private static int getCorrectIntValue(Scanner sc, String prompt, int min)  {
+        int i = 0;
+        boolean valid = false;
+        while(!valid)   {
+            i = getInt(sc, prompt);
+            if (i < min)   {
+                System.out.println("Error! number must be greater than the lowest value");
+            }
+            else{
+                valid = true;
+            }
+        }
+        return i;
     }
 }
