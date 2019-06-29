717964a6dd72549214f9d9df9343b4ce959a6506
MatthewCisler <matthew@cisler.com>
2019-06-28 22:45:47 -0500

    update game

--- GameFunctions.java
+++ GameFunctions.java
@@ -52,6 +52,106 @@ public class GameFunctions {
         System.out.printf("You have answered %d correctly and %d incorrectly.%n", correct, notCorrect);
     }
 
+    public static void MultiplicationTest() {
+        int answer;
+        int userAnswer;
+        String prompt = "Please enter the lowest value you would like to multiply today. ";
+        String highPrompt = "please enter the highest value you would like to multiply today. ";
+        final int NUMOFQUESTIONS = 10;
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.println();
+        System.out.println("This is the test session for using multiplication. To use this,");
+        System.out.println("give the lowest value and the highest value you would want to use to multiply");
+        System.out.println("then the amount of problems you would like to solve. At the end, the program");
+        System.out.println("will give the number of times you answered correctly and incorrectly.");
+        System.out.println("Along with a grade. There will be 10 Questions");
+        System.out.println();
+
+        int lowNumber = getInt(sc, prompt);
+        int highNumber = getCorrectIntValue(sc, highPrompt, lowNumber);
+
+        int correct = 0;
+        int notCorrect = 0;
+
+        for (int i = 0; i < NUMOFQUESTIONS; i++) {
+            int firstRand = rand.nextInt(highNumber - lowNumber) + lowNumber;
+            int secondRand = rand.nextInt(highNumber - lowNumber) + lowNumber;
+
+            answer = (firstRand * secondRand);
+            System.out.printf("Question %d out of 10%n", i + 1);
+            userAnswer = getInt(sc, "What is " + firstRand + " x " + secondRand + "? ");
+
+            if (userAnswer == answer) {
+                correct++;
+            } else {
+                notCorrect++;
+            }
+            userAnswer = 0;
+        }
+        System.out.printf("You have answered %d correctly and %d incorrectly.%n", correct, notCorrect);
+        double percentTotal = (correct / NUMOFQUESTIONS) * 100;
+        String ending;
+        System.out.println(percentTotal);
+        if (percentTotal < 59) {
+            ending = "Sorry, you didn't pass. Try again next time.";
+        } else {
+            ending = "Good job!";
+        }
+        System.out.printf("You got a %.2f, %s%n", percentTotal, ending);
+    }
+
+    public static void RockPaperScissors() {
+        System.out.println("This is Rock Paper Scissors, Rules are simple");
+        System.out.println("1: Rock");
+        System.out.println("2: Paper");
+        System.out.println("3: Scissors");
+        System.out.println();
+        System.out.println("Beat the computer. All you need to do");
+        int min = 0;
+        int max = 2;
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int userAns = getInt(sc, "Pick Rock, Paper, or Scissors");
+        int compAns = (rand.nextInt(max - min) + min) + 1;
+        System.out.println();
+        System.out.println(compAns);
+        String userAnsString;
+        String compAnsString;
+        String resultsTie = "Both Tied";
+        String resultsWon = "You won!";
+        String resultsLost = "You Lost!";
+
+        if (userAns == 1) {
+            userAnsString = "Rock";
+        } else if (userAns == 2) {
+            userAnsString = "Paper";
+        } else if (userAns == 3) {
+            userAnsString = "Scissors";
+        }
+
+        if (compAns == 1) {
+            compAnsString = "Rock";
+        } else if (compAns == 2) {
+            compAnsString = "Paper";
+        } else if (compAns == 3) {
+            compAnsString = "Scissors";
+        }
+
+        if (compAns == userAns) {
+            System.out.print(resultsTie);
+        } else if (userAns == 1 & compAns == 3) {
+            System.out.println(resultsWon);
+        } else if (userAns == 1 & compAns == 2) {
+            System.out.println(resultsLost);
+        }
+
+    }
+
     // Validations, do not put "Game" classes underneath these.
     private static int getInt(Scanner sc, String prompt) {
         int i = 0;
--- Main.java
+++ Main.java
@@ -1,46 +1,47 @@
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int choice;
         System.out.println("Welcome to the Math Facts game. To start, type out the ");
         System.out.println("number from the menu to pick a game. ");
         printMenu();
         System.out.print("What kind of math would you like to do today?: ");
-        choice = sc.nextInt();
+        int choice = sc.nextInt();
 
-        switch(choice)  {
-            case 1:
-                GameFunctions.MultiplicationPractice();
-                break;
-            case 2:
-                System.out.println("Function not available yet.");
-                break;
-            case 3:
-                System.out.println("Function not available yet.");
-                break;
-            case 4: 
-                System.out.println("Function not available yet.");
-                break;
-            case 5:
-                System.out.println("Function not available yet.");
-                break;  
-            case 6:
-                System.out.println("Function not available yet.");
-                break;
-            case 7:
-                System.out.println("Function not available yet.");
-                break;
-            case 8:
-                System.out.println("Function not available yet.");
-                break;
+        switch (choice) {
+        case 1:
+            GameFunctions.MultiplicationPractice();
+            break;
+        case 2:
+            GameFunctions.MultiplicationTest();
+            break;
+        case 3:
+            System.out.println("Function not available yet.");
+            break;
+        case 4:
+            System.out.println("Function not available yet.");
+            break;
+        case 5:
+            System.out.println("Function not available yet.");
+            break;
+        case 6:
+            System.out.println("Function not available yet.");
+            break;
+        case 7:
+            System.out.println("Function not available yet.");
+            break;
+        case 8:
+            System.out.println("Function not available yet.");
+            break;
+        case 9:
+            GameFunctions.RockPaperScissors();
         }
-
+        sc.close();
 
     }
-    private static void printMenu()    {
+
+    private static void printMenu() {
         System.out.println("1: Multiplication as practice");
         System.out.println("2: Multiplication as a test");
         System.out.println("3: Addition as practice");
@@ -49,6 +50,7 @@ public class Main {
         System.out.println("6: Subtraction as a test");
         System.out.println("7: Division as practice");
         System.out.println("8: Division as a test");
+        System.out.println("9: Rock, Paper, Scissors");
         System.out.println();
     }
 }
